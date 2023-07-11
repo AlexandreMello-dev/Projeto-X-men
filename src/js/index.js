39e512473420b1216972e685601069bfbd909b55
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-07-10 23:20:04 -0300

    projeto X-men

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-document.
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+        
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementyById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+    })    
+})
